254ac460862473a2e9fb86a4ecc3adc1616ce741
Yair Sovran <sovran@cs.nyu.edu>
2015-06-16 00:20:19 -0400

    Added a simple template and index.html view

--- web/scalica/micro/models.py
+++ web/scalica/micro/models.py
@@ -4,18 +4,20 @@ from django.db import models
 # Create your models here.
 # Take the users Table from https://docs.djangoproject.com/en/1.8/topics/auth/default/
 class Post(models.Model):
-    user = models.ForeignKey(User)
-    text = models.CharField(max_length=256, default="blah")
-    pub_date = models.DateTimeField('date_posted')
-    def __str__(self):
-        if len(self.text) < 16:
-            desc = self.text
-        else:
-            desc = textself.text[0:16]
-        return self.user.username + ':' + desc
+  user = models.ForeignKey(User)
+  text = models.CharField(max_length=256, default="blah")
+  pub_date = models.DateTimeField('date_posted')
+  def __str__(self):
+    if len(self.text) < 16:
+      desc = self.text
+    else:
+      desc = textself.text[0:16]
+    return self.user.username + ':' + desc
 
 class Following(models.Model):
-    follower = models.ForeignKey(User, related_name="user_follows")
-    followee = models.ForeignKey(User, related_name="user_followed")
-    follow_date = models.DateTimeField('follow data')
+  follower = models.ForeignKey(User, related_name="user_follows")
+  followee = models.ForeignKey(User, related_name="user_followed")
+  follow_date = models.DateTimeField('follow data')
+  def __str__(self):
+    return self.follower.username + "->" + self.followee.username
 
--- web/scalica/micro/views.py
+++ web/scalica/micro/views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Following
 
 # Create your views here.
 @login_required(redirect_field_name='my_redirect_field')
 def index(request):
-    return HttpResponse("Testing index page")
+    '''List of recent posts by people I follow'''
+    follows = [o.followee_id for o in Following.objects.filter(follower_id=1)]
+    # print Following.objects.filter(follower_id=1).query
+    followed_users = User.objects.filter(id__in=follows)
+    context = {'follows_list': followed_users}
+    return render(request, 'micro/index.html', context)
 
 def stream(request, user_id):
     return HttpResponse("Stream for user: %s." % user_id)
