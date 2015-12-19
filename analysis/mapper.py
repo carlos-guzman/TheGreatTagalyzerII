0416e035b748c492e538d0289b0f0ae3ab5c348b
batu <ba921@nyu.edu>
2015-12-19 14:31:06 -0500

    changed map function, dont forget to chmod +x

--- analysis/mapper.py
+++ analysis/mapper.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 import sys
-def sentimentAnalysis():
-    return(0.5)
 
 for line in sys.stdin:
     line = line.strip()
     words = line.split(',')
     text = words[2]
     hashtag_id = words[3]
-    sentiment_value = sentimentAnalysis(text)
+    sentiment_value = 0.5
     result = "%s,%s" % (str(hashtag_id), str(sentiment_value))
     sys.stdout.write(result)
