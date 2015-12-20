bb41fba53bc6936f14b7c11c800dd22d04f5d465
batu <ba921@nyu.edu>
2015-12-20 16:01:17 -0500

    updated the map reduce

--- analysis/keys.py
+++ analysis/keys.py
@@ -1,6 +1,8 @@
-S3_ACCESS_KEY = ''
-S3_SECRET_KEY = ''
-dbname = ''
-dbuser = ''
-dbhost = ''
-dbpass = ''
+S3_ACCESS_KEY = 'AKIAJAX3BHOTZJ2BBUVQ'
+S3_SECRET_KEY = 'BfBi7OYuRdQCd9X3QAJZqP58oR'
+dbname = 'tagalyzer'
+dbuser = 'taguser'
+dbhost = 'db.tagalyzer.io'
+dbpass = 'swordfish'
+
+
--- analysis/reducer.py
+++ analysis/reducer.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python
 import sys
 
+
+results = {}
+hashtag_ids = []
+
 def sentimentAnalysis(text):
     return(0,5)
 
-for line in sys.stdin:
-    # remove leading and trailing whitespace
+file = open("data.csv", 'r')
+for line in file:
     line = line.strip()
 
     # parse the input we got from mapper.py
-    hashtag_id = line.split(',')[0]
-    sentiment_values = line[line.index('[')+1 :line.index(']')].strip().split(',')
-    count = 0
-    hashtagSentiment = 0.0
-    for value in sentiment_values:
-        print(value.strip())
-        hashtagSentiment += float(value)
-        count += 1
+    hashtag_id, sentiment_value = line.split(",")
+    hashtag_id = int(hashtag_id[:-1])
+    sentiment_value = float(sentiment_value)
+    if hashtag_id not in hashtag_ids:
+        hashtag_ids.append(hashtag_id)
 
-    hashtagSentiment /= count
-    result = "%s,%s" % (str(hashtag_id), str(hashtagSentiment))
-    sys.stdout.write(result)
+    count = 1
+    if hashtag_id not in results:
+        results[hashtag_id] = [sentiment_value, count]
+    else:
+        count = results[hashtag_id][1] + 1
+        existingSentiment = results[hashtag_id][0]
+        average = (existingSentiment + sentiment_value) / count
+        results[hashtag_id] = [average, count]
+
+for hashtag in hashtag_ids:
+    print(hashtag, results[hashtag][0])
