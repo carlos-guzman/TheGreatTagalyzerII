e8f457ddcab81da15f2154211c2c9cd892deba50
hudavid <dh1635@nyu.edu>
2015-12-20 04:06:36 -0500

    more efficient db reads + cleanup

--- analysis/dataAnalysisDB.py
+++ analysis/dataAnalysisDB.py
@@ -12,13 +12,11 @@ shards = 4 #shards start from 1 to 4
 
 result = None
 
-#ok
 def connectIntoDB():
     DBcon = psycopg2.connect("dbname={} user={} host={} password={}".format(dbname, dbuser, dbhost, dbpass))
     cursor = DBcon.cursor()
     return DBcon
 
-#ok
 #execute arbitrary query (for debugging purposes)
 def execQuery(query, connection, cursor):
     cursor.execute(query)
@@ -27,7 +25,6 @@ def execQuery(query, connection, cursor):
     for row in rows:
         print(row)
 
-#ok
 #gets all posts which hasn't been analyzed yet
 def getNonAnalyzed(connection, cursor):
     columns = ['id', 'client_id', 'text', 'hashtag_id', 'sentiment_value', 'inserted_at', 'created_at']
@@ -42,22 +39,20 @@ def getNonAnalyzed(connection, cursor):
             file.write("\n");
             result = (dict(zip(columns, row)))
 
-#ok
 #given a post id, extract the post and create a dictionary
 def getValuesByID(postID, connection, cursor):
     columns = ['id', 'client_id', 'text', 'hashtag_id', 'sentiment_value', 'inserted_at', 'created_at']
 
-    for shardID in range(1,5):
-        cursor.execute("""SELECT * FROM shard_{0:07d}.posts WHERE id = '{i}';""".format(shardID, i=str(postID)) )
-        rows = cursor.fetchall()
+    shardID = postID / (2**31)
+    cursor.execute("""SELECT * FROM shard_{0:07d}.posts WHERE id = '{i}';""".format(shardID, i=str(postID)) )
+    rows = cursor.fetchall()
 
-        file = open(filename, 'w')
-        for row in rows:
-            print row
-            file.write(str(row));
-            result = (dict(zip(columns, row)))
+    file = open(filename, 'w')
+    for row in rows:
+        print row
+        file.write(str(row));
+        result = (dict(zip(columns, row)))
 
-#ok
 def uploadS3():
     S3conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,tls=True)
     f = open(filename,'rb')
@@ -65,19 +60,17 @@ def uploadS3():
 
 #update the sentiment value of the post with the given post ID
 def updatePostSentiment(sentimentValue, postID, connection, cursor):
-    shardID = (postID % shards) + 1
+    shardID = postID / (2**31)
     cursor.execute("""UPDATE shard_{0:07d}.posts SET sentiment_value = {s} WHERE id = {i} ;""".format(shardID, s=str(sentimentValue), i=str(postID) ) )
     connection.commit()
     print "Updated post {} with the sentiment {}".format(str(postID), str(sentimentValue))
 
-#ok
 #update the sentiment value of the given hashtag
 def updateHashtagSentiment(combinedSentimentValue, hashtag, connection, cursor):
     shardID = (java_string_hashcode(hashtag) % shards) + 1
     cursor.execute("""UPDATE shard_{0:07}.hashtags SET sentiment_value = {s} WHERE name = '{t}';""".format(shardID, s=str(combinedSentimentValue), t=hashtag) )
     connection.commit()
 
-#ok
 #hashing function used for sharding purposes
 def java_string_hashcode(s):
     h = 0
@@ -99,12 +92,6 @@ def main():
     try:
         DBcon = connectIntoDB()
         cursor = DBcon.cursor()
-        #updateHashtagSentiment(-0.5, 'asdf', DBcon, cursor)
-        tp = "test post from david"
-        #printEverything(DBcon, cursor)
-
-        getValuesByID(8589934593L, DBcon, cursor)
-        #updatePostSentiment(-0.1, 8589934593L, DBcon, cursor)
         #getNonAnalyzed(DBcon, cursor)
         #uploadS3()
 
